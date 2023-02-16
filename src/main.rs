c8a0dd33bebdb907b59b992abe53258e2dfae6d9
whx828 <whx19990828@gmail.com>
2023-02-16 16:57:51 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter, Read, Write};
+use clap::Parser;
+
+
+/// Obsidian md format
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the markdown file to format
+    #[arg(short, long)]
+    file: Option<String>,
+}
+
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+
+    match args.file {
+        None => {
+            println!("No markdown file specified. Please specify your markdown file.");
+            Ok(())
+        }
+        Some(program_name) => run_file(&program_name),
+    }
+}
+
+fn run_file(path: &str) -> io::Result<()> {
+    let file = File::open(path)?;
+    let mut reader = BufReader::new(file);
+
+    // 读取文件内容
+    let mut content = String::new();
+    reader.read_to_string(&mut content)?;
+    let mut new_content = String::new();
+
+    // 修改文件内容
+    for line in content.lines() {
+        if !is_chinese(&line) {
+            let new_line = line.replace("“", "\"").replace("”", "\"").replace("’", "\'").replace("‘", "\'");
+            new_content.push_str(&new_line);
+            new_content.push_str("\n");
+        } else {
+            new_content.push_str(line);
+            new_content.push_str("\n");
+        }
+    }
+
+    // 将修改后的文件内容写回到文件中
+    let file = File::create(path)?;
+    let mut writer = BufWriter::new(file);
+    writer.write_all(new_content.as_bytes())?;
+
+    Ok(())
+}
+
+fn is_chinese(s: &str) -> bool {
+    s.trim().chars().any(|c| c >= '\u{4E00}' && c <= '\u{9FFF}')
+}
